22ccdc3f843778ba29c9d3a23fd23a1799ea6d9a
Thomas Liao <tjl83@case.edu>
2014-10-27 17:10:26 -0400

    Chat in C#

---
+++ TinfoilChat/Chatography/Chatography/Client.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatography
+{
+
+    class Contact
+    {
+        List<string> ip;
+        int port;
+        TcpClient connection;
+
+        public List<string> lastKnownIPAddresses()
+        {
+            return ip;
+        }
+
+        public int getPort()
+        {
+            return port;
+        }
+
+        public void setConnection(TcpClient client)
+        {
+            connection = client;
+        }
+    }
+
+    class Client
+    {
+        List<Contact> contacts;
+        List<Contact> online = new List<Contact>();
+        NetworkStream serverStream;
+
+        public Client()
+        {
+
+        }
+
+        public void initialize()
+        {
+            TcpClient client = new TcpClient();
+            foreach (Contact contact in contacts)
+            {
+                if (connectTo(client, contact))
+                {
+                    online.Add(contact);
+                }
+            }
+
+            TcpListener serverSocket = new TcpListener(420);
+            while(true){
+                online.Add(serverSocket.AcceptTcpClient());
+            }
+        }
+
+        private Boolean connectTo(TcpClient client, Contact contact)
+        {
+            foreach (string ip in contact.lastKnownIPAddresses())
+            {
+                try
+                {
+                    client.Connect(ip, contact.getPort());
+                    contact.setConnection(client);
+                    return true;
+                }
+                catch (SocketException e)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
