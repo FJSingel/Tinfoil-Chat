9e0e2a98cdefa3426cc05711a7a18d4579e1243d
Thomas Liao <tjl83@case.edu>
2014-10-27 20:58:36 -0400

    Rough Testing Phase

--- TinfoilChat/Chatography/Chatography/Client.cs
+++ TinfoilChat/Chatography/Chatography/Client.cs
@@ -2,84 +2,177 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Chatography
 {
-
-    class Contact
-    {
-        List<string> ip;
-        int port;
-        TcpClient connection;
-
-        public List<string> lastKnownIPAddresses()
-        {
-            return ip;
-        }
-
-        public int getPort()
-        {
-            return port;
-        }
-
-        public void setConnection(TcpClient client)
-        {
-            connection = client;
-        }
-    }
-
     class Client
     {
-        List<Contact> contacts;
-        List<Contact> online = new List<Contact>();
-        NetworkStream serverStream;
+        List<TcpClient> onlineClients;
+        NetworkStream stream;
+
+        static bool isOnline;
 
         public Client()
         {
-
+            isOnline = true;
         }
 
         public void initialize()
         {
-            TcpClient client = new TcpClient();
-            foreach (Contact contact in contacts)
+            Thread comThread = new Thread(waitForCommand);
+            comThread.Start();
+
+            waitForConnection();
+        }
+
+        public void waitForCommand()
+        {
+            while (isOnline)
             {
-                if (connectTo(client, contact))
+                string commands = ">> Commands are:\n>>  /find\t-Find user at specified IP address and Port Number.\n" +
+                                                    ">>  /exit\t-To go offline and exit.\n" + 
+                                                    ">>  /help\tdisplay commands.";
+                Console.WriteLine(commands);
+                string input = Console.ReadLine();
+                if (input[0] == '/')
                 {
-                    online.Add(contact);
+                    string command = input.Substring(1);
+                    switch (command)
+                    {
+                        case "find":
+                            findUser();
+                            break;
+                        case "exit":
+                            isOnline = false;
+                            break;
+                        case "help":
+                            Console.WriteLine(commands);
+                            break;
+                        default:
+                            Console.WriteLine(">> Unrecognized Command. Type 'help' for a list of commands.");
+                            break;
+                    }
+                }
+                else if (input[0] == '@')
+                {
+
+                }
+                else
+                {
+                    broadcast(input);
                 }
             }
 
-            TcpListener serverSocket = new TcpListener(420);
-            while(true){
-                online.Add(serverSocket.AcceptTcpClient());
-            }
+            Console.WriteLine("Exiting");
         }
 
-        private Boolean connectTo(TcpClient client, Contact contact)
+        private void findUser()
         {
-            foreach (string ip in contact.lastKnownIPAddresses())
+            string ip = "0.0.0.0";
+            int port = 420;
+            bool failed = true;
+            while (failed)
             {
                 try
                 {
-                    client.Connect(ip, contact.getPort());
-                    contact.setConnection(client);
-                    return true;
+                    Console.WriteLine("Enter IP Address");
+                    IPAddress IPADDRESS;
+                    String line = Console.ReadLine();
+                    if (IPAddress.TryParse(line, out IPADDRESS))
+                        ip = line;
+                    failed = false;
                 }
-                catch (SocketException e)
+                catch (Exception ex)
                 {
-                    return false;
+                    Console.WriteLine(">> " + ex.ToString());
                 }
             }
-            return false;
+            failed = true;
+            while (failed)
+            {
+                try
+                {
+                    Console.WriteLine("Enter port number");
+                    port = Int32.Parse(Console.ReadLine());
+                    failed = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(">> " + ex.ToString());
+                }
+            }
+            TcpClient nextClient = new TcpClient(ip, port);
+            addClient(nextClient);
         }
 
-        static void Main(string[] args)
+        private void waitForConnection()
         {
+            TcpListener serverSocket = new TcpListener(420);
 
+            serverSocket.Start();
+
+            while (isOnline)
+            {
+                TcpClient nextClient = serverSocket.AcceptTcpClient();
+                addClient(nextClient);
+            }
+        }
+
+        private void addClient(TcpClient client)
+        {
+            string clientNo = Convert.ToString(onlineClients.Count);
+            Console.WriteLine(" >> " + "Client-" + clientNo + " online!");
+            onlineClients.Add(client);
+
+            Thread cThread = new Thread(() => clientListener(client,clientNo));
+            cThread.Start();
+        }
+
+        private void clientListener(TcpClient clSocket, string clNo)
+        {
+            string dataFromClient = null;
+            byte[] bytesFrom = new byte[10025];
+            NetworkStream networkStream;
+
+            while ((true))
+            {
+                try
+                {
+                    networkStream = clSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, clSocket.ReceiveBufferSize);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine(" Client-" + clNo + ":" + dataFromClient);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" >> " + ex.ToString());
+                }
+            }
+        }
+
+        private void broadcast(string msg)
+        {
+            byte[] bytesToEveryone = new byte[10025];
+            NetworkStream networkStream;
+
+            foreach (TcpClient client in onlineClients)
+            {
+                networkStream = client.GetStream();
+                bytesToEveryone = System.Text.Encoding.ASCII.GetBytes(msg);
+                int bytesOfmsg = System.Text.Encoding.ASCII.GetByteCount(msg);
+                networkStream.Write(bytesToEveryone, 0, bytesOfmsg);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.initialize();
         }
     }
 }
